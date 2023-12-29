99845502937562bf0cee229c7ad2ec8dd36f810f
icathianrain <2205794866@qq.com>
2023-12-29 12:01:45 +0800

    ✨ feat(lab2.e6): finish lab2.e6
    完成 lab2 Exercise6
    实现 `Limit` 操作

--- godb/limit_op.go
+++ godb/limit_op.go
@@ -10,13 +10,13 @@ type LimitOp struct {
 // lim is how many tuples to return and child is the child op.
 func NewLimitOp(lim Expr, child Operator) *LimitOp {
 	// TODO: some code goes here
-	return nil
+	return &LimitOp{child, lim} //replace me
 }
 
 // Return a TupleDescriptor for this limit
 func (l *LimitOp) Descriptor() *TupleDesc {
 	// TODO: some code goes here
-	return nil
+	return l.child.Descriptor() //replace me
 
 }
 
@@ -25,5 +25,31 @@ func (l *LimitOp) Descriptor() *TupleDesc {
 // [lim] tuples it sees (where lim is specified in the constructor).
 func (l *LimitOp) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
 	// TODO: some code goes here
-	return nil, nil
+	// 获取limit的值
+	nValue, err := l.limitTups.EvalExpr(nil)
+	if err != nil {
+		return nil, nil
+	}
+	n := nValue.(IntField).Value
+	// 获取child的迭代器
+	iter, err := l.child.Iterator(tid)
+	if err != nil {
+		return nil, err
+	}
+	// 返回一个函数，该函数每次调用都会返回一个tuple
+	i := 0
+	return func() (*Tuple, error) {
+		// 如果i大于等于n，说明已经返回了n个tuple，返回nil
+		if i >= int(n) {
+			return nil, nil
+		}
+		// 否则，调用child的迭代器，返回一个tuple
+		tup, err := iter()
+		if err != nil {
+			return nil, err
+		}
+		i++
+		return tup, nil
+	}, nil
+
 }
