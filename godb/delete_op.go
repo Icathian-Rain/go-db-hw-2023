c5cff2a20606e800996936742b4d67fb26b724aa
icathianrain <2205794866@qq.com>
2023-12-29 12:00:16 +0800

    ✨ feat(lab2.e3): finish lab2.e3
    完成 lab2 Exercise3
    实现 `insert` 和 `delete` 操作

--- godb/delete_op.go
+++ godb/delete_op.go
@@ -2,19 +2,28 @@ package godb
 
 type DeleteOp struct {
 	// TODO: some code goes here
+	deleteFile DBFile     // 删除的文件
+	child      Operator   // 输入的操作符
+	desc       *TupleDesc // 描述符
 }
 
 // Construtor.  The delete operator deletes the records in the child
 // Operator from the specified DBFile.
 func NewDeleteOp(deleteFile DBFile, child Operator) *DeleteOp {
 	// TODO: some code goes here
-	return nil
+	// 返回一个删除操作符
+	return &DeleteOp{deleteFile, child, &TupleDesc{
+		Fields: []FieldType{
+			{Fname: "count", Ftype: IntType},
+		},
+	}}
 }
 
 // The delete TupleDesc is a one column descriptor with an integer field named "count"
 func (i *DeleteOp) Descriptor() *TupleDesc {
 	// TODO: some code goes here
-	return nil
+	// 返回一个描述符，包含一个字段，名为count，类型为int，统计删除的数量
+	return i.desc
 
 }
 
@@ -25,6 +34,37 @@ func (i *DeleteOp) Descriptor() *TupleDesc {
 // method.
 func (dop *DeleteOp) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
 	// TODO: some code goes here
-	return nil, nil
+	// 返回一个迭代器
+	iter, err := dop.child.Iterator(tid)
+	if err != nil {
+		return nil, nil
+	}
+	count := 0
 
+	return func() (*Tuple, error) {
+		for {
+			// 迭代
+			tuple, err := iter()
+			if err != nil {
+				return nil, err
+			}
+			if tuple == nil {
+				break
+			}
+			// 删除
+			err = dop.deleteFile.deleteTuple(tuple, tid)
+			if err != nil {
+				return nil, err
+			}
+			// 计数
+			count++
+		}
+		// 返回一个只有一个字段的元组，字段为count，类型为int
+		return &Tuple{
+			Desc: *dop.desc,
+			Fields: []DBValue{
+				IntField{int64(count)},
+			},
+		}, nil
+	}, nil
 }
--- godb/insert_op.go
+++ godb/insert_op.go
@@ -3,19 +3,28 @@ package godb
 // TODO: some code goes here
 type InsertOp struct {
 	// TODO: some code goes here
+	insertFile DBFile     // 插入的文件
+	child      Operator   // 被插入的操作符
+	desc       *TupleDesc // 描述符
 }
 
 // Construtor.  The insert operator insert the records in the child
 // Operator into the specified DBFile.
 func NewInsertOp(insertFile DBFile, child Operator) *InsertOp {
 	// TODO: some code goes here
-	return nil
+	// 返回一个插入操作符
+	return &InsertOp{insertFile, child, &TupleDesc{
+		Fields: []FieldType{
+			{Fname: "count", Ftype: IntType},
+		},
+	}}
 }
 
 // The insert TupleDesc is a one column descriptor with an integer field named "count"
 func (i *InsertOp) Descriptor() *TupleDesc {
 	// TODO: some code goes here
-	return nil
+	// 返回一个描述符，包含一个字段，名为count，类型为int，统计插入的数量
+	return i.desc
 }
 
 // Return an iterator function that inserts all of the tuples from the child
@@ -25,6 +34,37 @@ func (i *InsertOp) Descriptor() *TupleDesc {
 // method.
 func (iop *InsertOp) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
 	// TODO: some code goes here
-	return nil, nil
-
+	// 返回一个迭代器
+	iter, err := iop.child.Iterator(tid)
+	if err != nil {
+		return nil, nil
+	}
+	// 计数
+	count := 0
+	return func() (*Tuple, error) {
+		for {
+			// 迭代
+			tuple, err := iter()
+			if err != nil {
+				return nil, err
+			}
+			if tuple == nil {
+				break
+			}
+			// 插入
+			err = iop.insertFile.insertTuple(tuple, tid)
+			if err != nil {
+				return nil, err
+			}
+			// 计数
+			count++
+		}
+		// 返回结果元组，包含插入的数量
+		return &Tuple{
+			Desc: *iop.desc,
+			Fields: []DBValue{
+				IntField{int64(count)},
+			},
+		}, nil
+	}, nil
 }
