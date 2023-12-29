75d242e9558fde35da4b170be0b15e3225d43fc7
icathianrain <2205794866@qq.com>
2023-12-29 11:58:26 +0800

    ✨ feat(lab2.e1): finish lab2.e1
    完成 lab2 Exercise1
    实现 `filter` 和 `join` 操作

--- godb/filter_op.go
+++ godb/filter_op.go
@@ -3,11 +3,11 @@ package godb
 import "golang.org/x/exp/constraints"
 
 type Filter[T constraints.Ordered] struct {
-	op     BoolOp
-	left   Expr
-	right  Expr
-	child  Operator
-	getter func(DBValue) T
+	op     BoolOp          // 比较符号
+	left   Expr            // 左边的表达式
+	right  Expr            // 右边的表达式
+	child  Operator        // HeapFile, 用来获取迭代器与TupleDescriptor
+	getter func(DBValue) T // 获取值的函数
 }
 
 func intFilterGetter(v DBValue) int64 {
@@ -49,7 +49,7 @@ func newFilter[T constraints.Ordered](constExpr Expr, op BoolOp, field Expr, chi
 // Return a TupleDescriptor for this filter op.
 func (f *Filter[T]) Descriptor() *TupleDesc {
 	// TODO: some code goes here
-	return nil
+	return f.child.Descriptor()
 }
 
 // Filter operator implementation. This function should iterate over
@@ -58,5 +58,45 @@ func (f *Filter[T]) Descriptor() *TupleDesc {
 // HINT: you can use the evalPred function defined in types.go to compare two values
 func (f *Filter[T]) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
 	// TODO: some code goes here
-	return nil, nil
+	// 若左边表达式的类型不等于右边表达式的类型，则返回错误
+	if f.left.GetExprType().Ftype != f.right.GetExprType().Ftype {
+		return nil, GoDBError{IncompatibleTypesError, "cannot apply filter to non matching types"}
+	}
+	// 获取迭代器
+	iter, err := f.child.Iterator(tid)
+	if err != nil {
+		return nil, err
+	}
+	// 右表达式为定值，提前计算
+	rightVal, err := f.right.EvalExpr(nil)
+	if err != nil {
+		return nil, err
+	}
+	// 迭代器为空，返回空
+	if iter == nil {
+		return nil, nil
+	}
+	// 返回迭代函数
+	return func() (*Tuple, error) {
+		for {
+			// 获取下一个tuple
+			tuple, err := iter()
+			if err != nil {
+				return nil, err
+			}
+			if tuple == nil {
+				break
+			}
+			// 获取左右表达式的值
+			leftVal, err := f.left.EvalExpr(tuple)
+			if err != nil {
+				return nil, err
+			}
+			// 比较左右表达式的值
+			if evalPred[T](f.getter(leftVal), f.getter(rightVal), f.op) {
+				return tuple, nil
+			}
+		}
+		return nil, nil
+	}, nil
 }
--- godb/join_op.go
+++ godb/join_op.go
@@ -3,12 +3,14 @@ package godb
 type EqualityJoin[T comparable] struct {
 	// Expressions that when applied to tuples from the left or right operators,
 	// respectively, return the value of the left or right side of the join
-	leftField, rightField Expr
+	leftField, rightField Expr // 左值，右值
 
+	// 迭代器
 	left, right *Operator //operators for the two inputs of the join
 
 	// Function that when applied to a DBValue returns the join value; will be
 	// one of intFilterGetter or stringFilterGetter
+	// 获取值
 	getter func(DBValue) T
 
 	// The maximum number of records of intermediate state that the join should use
@@ -52,7 +54,9 @@ func NewStringJoin(left Operator, leftField Expr, right Operator, rightField Exp
 // HINT: use the merge function you implemented for TupleDesc in lab1
 func (hj *EqualityJoin[T]) Descriptor() *TupleDesc {
 	// TODO: some code goes here
-	return nil
+	leftDesc := (*hj.left).Descriptor()
+	rightDesc := (*hj.right).Descriptor()
+	return leftDesc.merge(rightDesc)
 }
 
 // Join operator implementation.  This function should iterate over the results
@@ -70,7 +74,86 @@ func (hj *EqualityJoin[T]) Descriptor() *TupleDesc {
 // out.  To pass this test, you will need to use something other than a nested
 // loops join.
 func (joinOp *EqualityJoin[T]) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
-
 	// TODO: some code goes here
-	return nil, nil
+	if joinOp.leftField.GetExprType().Ftype != joinOp.rightField.GetExprType().Ftype {
+		return nil, GoDBError{TypeMismatchError, "can't join fields of different types"}
+	}
+
+	var retFunc func() (*Tuple, error)
+	// 方法一，暴力遍历
+	leftIter, _ := (*joinOp.left).Iterator(tid)
+	rightIter, _ := (*joinOp.right).Iterator(tid)
+	leftTuple, _ := leftIter()
+	rightTuple, _ := rightIter()
+	retFunc = func() (*Tuple, error) {
+		for {
+			if leftTuple == nil {
+				break
+			}
+			for {
+				if rightTuple == nil {
+					break
+				}
+				// 比较左右两个值
+				leftValue, _ := joinOp.leftField.EvalExpr(leftTuple)
+				rightValue, _ := joinOp.rightField.EvalExpr(rightTuple)
+				if joinOp.getter(leftValue) == joinOp.getter(rightValue) {
+					resTuple := joinTuples(leftTuple, rightTuple)
+					// 开启下一轮right循环
+					rightTuple, _ = rightIter()
+					return resTuple, nil
+				} else {
+					rightTuple, _ = rightIter()
+				}
+			}
+			leftTuple, _ = leftIter()
+			rightIter, _ = (*joinOp.right).Iterator(tid)
+			rightTuple, _ = rightIter()
+		}
+		return nil, nil
+	}
+	return retFunc, nil
+	// 方法二
+	// 读取右表的所有元组，将其放入哈希表中
+	// rightIter, _ := (*joinOp.right).Iterator(tid)
+	// rightHash := make(map[T][]*Tuple)
+	// for {
+	// 	rightTuple, _ := rightIter()
+	// 	if rightTuple == nil {
+	// 		break
+	// 	}
+	// 	rightValue, _ := joinOp.rightField.EvalExpr(rightTuple)
+	// 	rightHash[joinOp.getter(rightValue)] = append(rightHash[joinOp.getter(rightValue)], rightTuple)
+	// }
+	// // 遍历左表
+	// leftIter, _ := (*joinOp.left).Iterator(tid)
+	// // 初始化第一次迭代
+	// leftTuple, _ := leftIter()
+	// leftValue, _ := joinOp.leftField.EvalExpr(leftTuple)
+	// rightTuples := rightHash[joinOp.getter(leftValue)]
+	// rightNum := 0
+	// var rightTuple *Tuple
+	// retFunc = func() (*Tuple, error) {
+	// 	for {
+	// 		for {
+	// 			if rightNum >= len(rightTuples) {
+	// 				break
+	// 			}
+	// 			rightTuple = rightTuples[rightNum]
+	// 			rightNum++
+	// 			retTuple := joinTuples(leftTuple, rightTuple)
+	// 			return retTuple, nil
+	// 		}
+	// 		// 开启下一轮left循环
+	// 		leftTuple, _ = leftIter()
+	// 		if leftTuple == nil {
+	// 			break
+	// 		}
+	// 		leftValue, _ = joinOp.leftField.EvalExpr(leftTuple)
+	// 		rightTuples = rightHash[joinOp.getter(leftValue)]
+	// 		rightNum = 0
+	// 	}
+	// 	return nil, nil
+	// }
+	// return retFunc, nil
 }
